5ff470240cdb03a00308c017931d544c166b8c61
Anders Kalhauge <anders@kalhauge.dk>
2019-04-16 12:01:41 +0200

    distance to light

--- src/main/kotlin/info/itseminar/lego/server/Train.kt
+++ src/main/kotlin/info/itseminar/lego/server/Train.kt
@@ -2,8 +2,15 @@ package info.itseminar.lego.server
 
 import java.lang.Math.max
 import java.lang.Math.min
+import java.util.*
 
 class Train(val id: Int, val world: World) : Runnable {
+    private val GREEN_GREEN: UByte = 6u
+    private val RED_GREEN: UByte = 3u
+    private val RED: UByte = 1u
+    private var nextLight: UByte = GREEN_GREEN
+    private val random = Random()
+    private var distanceToLight = 0
     var actualSpeed = 0
     var targetSpeed = 0
     var isBreaking: Boolean = false
@@ -21,10 +28,19 @@ class Train(val id: Int, val world: World) : Runnable {
         while (service != null) {
             val accelleration = if (isBreaking) 50 else 10
             Thread.sleep(500)
+            if (distanceToLight <= 0) {
+              distanceToLight = random.nextInt(2000) + 1000
+              nextLight = when (nextLight) {
+                GREEN_GREEN -> RED_GREEN
+                RED_GREEN -> RED
+                else -> GREEN_GREEN
+                }
+              }
+            distanceToLight -= actualSpeed
             if (targetSpeed > actualSpeed) actualSpeed = min(actualSpeed + accelleration, targetSpeed)
             if (targetSpeed < actualSpeed) actualSpeed = max(actualSpeed - accelleration, targetSpeed)
             if (actualSpeed == 0) isBreaking = false
-            service?.information(actualSpeed, 0, 0, 1u)
+            service?.information(actualSpeed, 0, distanceToLight/100, nextLight)
             }
         }
 
